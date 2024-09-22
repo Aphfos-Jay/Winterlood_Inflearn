263c1e47196a04ec5ef44df8f74237a681e2a504
Jay <aphfos.jjw@gmail.com>
2024-09-22 22:11:22 +0900

    Section12 Fifth Commit

---
+++ onebite-react/section12 - An Emotional Diary/src/components/Editor.jsx
@@ -0,0 +1,114 @@
+import "./Editor.css";
+import EmotionItem from "./EmotionItem";
+import Button from "../components/Button";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const emotionList = [
+  { emotionId: 1, emotionName: "완전 좋음" },
+  { emotionId: 2, emotionName: "좋음" },
+  { emotionId: 3, emotionName: "보통" },
+  { emotionId: 4, emotionName: "나쁨" },
+  { emotionId: 5, emotionName: "완전 나쁨" },
+];
+
+const getStringedDate = (targetDate) => {
+  //날짜 -> YYYY-MM-DD
+  let year = targetDate.getFullYear();
+  let month = targetDate.getMonth() + 1;
+  let date = targetDate.getDate();
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (date < 10) {
+    date = `0${date}`;
+  }
+
+  return `${year}-${month}-${date}`;
+};
+
+const Editor = ({ onSubmit }) => {
+  const [input, setInput] = useState({
+    createdDate: new Date(),
+    emotionId: 3,
+    content: "",
+  });
+  const nav = useNavigate();
+  const onChangeInput = (e) => {
+    console.log(e.target.name);
+    console.log(e.target.value);
+
+    let name = e.target.name;
+    let value = e.target.value;
+
+    if (name === "createdDate") {
+      value = new Date(value);
+    }
+
+    setInput({
+      ...input,
+      [name]: value,
+    });
+  };
+
+  const onClickSubmitButton = () => {
+    // 에딧페이지가 최초생성, 수정페이지 공통으로 사용하기 때문에 Editor 페이지를 호출 하는 부분에서
+    // 이게 create인이 update인지 구분지어주게끔 onSubmit 함수를 통해 해당 내용도 전달하게끔 하여
+    // Button 클릭시 create or update를 알아서 수행하게끔 만들어준다.
+    onSubmit(input);
+  };
+
+  return (
+    <div className="Editor">
+      <section className="date_section">
+        <h4>오늘의 날짜</h4>
+        <input
+          name="createdDate"
+          onChange={onChangeInput}
+          value={getStringedDate(input.createdDate)}
+          type="date"
+        />
+      </section>
+      <section className="emotion_section">
+        <h4>오늘의 감정</h4>
+        <div className="emotion_list_wrapper">
+          {emotionList.map((item) => (
+            <EmotionItem
+              onClick={() =>
+                onChangeInput({
+                  target: {
+                    name: "emotionId",
+                    value: item.emotionId,
+                  },
+                })
+              }
+              key={item.emotionId}
+              {...item}
+              isSelected={item.emotionId === input.emotionId}
+            />
+          ))}
+        </div>
+      </section>
+      <section className="content_section">
+        <h4>오늘의 일기</h4>
+        <textarea
+          name="content"
+          value={input.content}
+          onChange={onChangeInput}
+          placeholder="오늘은 어땠나요?"
+        />
+      </section>
+      <section className="button_section">
+        <Button onClick={() => nav(-1)} text={"취소하기"} />
+        <Button
+          onClick={onClickSubmitButton}
+          text={"작성완료"}
+          type={"POSITIVE"}
+        />
+      </section>
+    </div>
+  );
+};
+
+export default Editor;
--- onebite-react/section12 - An Emotional Diary/src/pages/New.jsx
+++ onebite-react/section12 - An Emotional Diary/src/pages/New.jsx
@@ -1,5 +1,29 @@
+import Header from "../components/Header";
+import Button from "../components/Button";
+import Editor from "../components/Editor";
+import { useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { DiaryDispatchContext } from "../App";
+
 const New = () => {
-  return <div>New</div>;
+  const nav = useNavigate();
+  // nav(-1) 이면 페이지 뒤로가기 기능 구현
+
+  const { onCreate } = useContext(DiaryDispatchContext);
+  const onSubmit = (input) => {
+    onCreate(input.createdDate.getTime(), input.emotionId, input.content);
+    nav("/", { replace: true }); //replace: 뒤로가기 방지하면서 페이지를 이동시킴
+  };
+
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftchild={<Button text={"< 뒤로 가기"} onClick={() => nav(-1)} />}
+      />
+      <Editor onSubmit={onSubmit} />
+    </div>
+  );
 };
 
 export default New;
