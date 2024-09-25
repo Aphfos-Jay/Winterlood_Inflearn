ec222e588ef54734a72f36a301df913faf3ed069
Jay <aphfos.jjw@gmail.com>
2024-09-25 11:03:15 +0900

    Section12 Completed

--- onebite-react/section12 - An Emotional Diary/src/App.jsx
+++ onebite-react/section12 - An Emotional Diary/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { useReducer, useRef, createContext, useEffect, useState } from "react";
 import { Routes, Route, Link, useNavigate, json } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Diary from "./pages/Diary";
 import New from "./pages/New";
 import NotFound from "./pages/NotFound";
